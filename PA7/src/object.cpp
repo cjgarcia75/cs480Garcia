0b25cdc3210b3028ab82e0e1bbee636661e8640d
cjgarcia75 <44075677+cjgarcia75@users.noreply.github.com>
2019-10-13 09:38:47 -0700

    Add files via upload

--- PA7/src/object.cpp
+++ PA7/src/object.cpp
@@ -56,6 +56,7 @@ void Object::Update(unsigned int dt)
   else
   {
     model = glm::translate(glm::mat4(1.0f), glm::vec3((radius * cos(angle)), 0.0, (radius * sin(angle))));
+    
     tranModel = model;
   }
   model = glm::rotate(model, glm::radians(tilt), glm::vec3(0.0, 0.0, 1.0));
